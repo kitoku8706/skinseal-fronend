5de523f9f577b9d183120c7c7e9170a03e2c2e92
kitoku8706 <kitoku8706@gmail.com>
2025-10-03 18:52:37 +0900

    NavBar 스타일 추가 및 관련 CSS 파일 생성

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0', // 모든 IP에서 접근 허용
+    port: 5173,      // (원하는 포트)
     proxy: {
-      '/api': 'http://localhost:8090'
+      '/api': 'http://localhost:8080'
     }
   }
+  // server: {
+  //   proxy: {
+  //     '/api': 'http://localhost:8080'
+  //   }
+  // }
 })
