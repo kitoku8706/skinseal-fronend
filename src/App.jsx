9e52a81667f3e1d52a2d1735bc6e2e6dcfaed9a1
kitoku8706 <kitoku8706@gmail.com>
2025-10-14 00:27:57 +0900

    공지사항 수정 페이지 추가 및 조회수 관리 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import RegisterModal from "./components/RegisterModal.jsx";
 import RightSidebar from "./components/RightSidebar";
 import IntroPage from "./pages/IntroPage";
 import { NoticeForm } from './components/NoticeForm';
+import NoticeEditPage from "./pages/NoticeEditPage";
 import ManagementTeam from "./pages/ManagementTeam";
 import Directions from "./pages/Directions";
 import ReservationConsultPage from "./pages/ReservationConsultPage";
@@ -134,6 +135,7 @@ function App() {
           <Route path="/diagnosis" element={<DiagnosisPage />} />
           <Route path="/diagnosis/:id" element={<DiagnosisPage />} />
           <Route path="/notice/write" element={<NoticeForm />} />
+          <Route path="/notice/edit/:id" element={<NoticeEditPage />} />
           <Route path="/mypage" element={<MyPage />} />
 
         </Routes>
